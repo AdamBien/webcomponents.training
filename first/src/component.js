086e063da5f69f74571bf6debc567600ce1fe0a6
Adam Bien <abien@adam-bien.com>
2017-12-30 12:39:19 +0100

    customelement upgrade

--- first/src/component.js
+++ first/src/component.js
@@ -18,17 +18,19 @@ class ADuke extends HTMLElement {
     }
 
 }
-
-const shouldNotExist = customElements.get("a-duke");
-console.log(shouldNotExist);
-
 customElements.whenDefined("a-duke").
     then(_ => console.log(customElements.get("a-duke")));
 
-console.log("before");
-customElements.define("a-duke", ADuke);
-console.log("after");
+document.querySelector("#register").onclick = _ => {
+    console.log("before");
+    customElements.define("a-duke", ADuke);
+    console.log("after");
+}    
 
-const el = document.querySelector("a-duke");
-document.body.remove(el);
+
+document.querySelector("#remove").onclick = _ => { 
+    document.body.remove(document.querySelector("a-duke"));
+
+}
+
 
