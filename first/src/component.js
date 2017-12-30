e5ba97ef582315686818e0e17790f7316aee6024
Adam Bien <abien@adam-bien.com>
2017-12-30 13:12:35 +0100

    templates and custom elements

--- first/src/component.js
+++ first/src/component.js
@@ -1,36 +1,26 @@
+const template = document.createElement('template');
+template.innerHTML = `
+    <p>
+    hey from template
+    </p>
+`;
+
 class ADuke extends HTMLElement { 
 
     constructor() { 
         super();
         console.log("constructor");
-        this.innerText = this.innerText +  " hey from js";
     }
 
     connectedCallback() { 
         console.log("connected");
+        this.appendChild(template.content.cloneNode(true));
+        this.appendChild(template.content.cloneNode(true));
+        this.appendChild(template.content.cloneNode(true));
     }
-    disconnectedCallback() {
-        console.log("disconnected");
-     }
     
-    adoptedCallback(oldDoc, newDoc) { 
-        console.log(oldDoc,newDoc);
-    }
 
 }
-customElements.whenDefined("a-duke").
-    then(_ => console.log(customElements.get("a-duke")));
-
-document.querySelector("#register").onclick = _ => {
-    console.log("before");
-    customElements.define("a-duke", ADuke);
-    console.log("after");
-}    
-
-
-document.querySelector("#remove").onclick = _ => { 
-    document.body.remove(document.querySelector("a-duke"));
-
-}
+customElements.define("a-duke", ADuke);
 
 
