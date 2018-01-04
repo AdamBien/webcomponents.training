8dab8c6703f9c400390ea51524b6f4bf050effbf
Adam Bien <abien@adam-bien.com>
2018-01-04 15:41:36 +0100

    slots and templates for customizing the header and footer

--- blog/src/blog.js
+++ blog/src/blog.js
@@ -9,14 +9,34 @@ class ABlog extends HTMLElement {
         return this.getAttribute('number') || 1;
     }
 
+    header() { 
+        const template = document.createElement('template');
+        template.innerHTML = `
+        <slot name="header">B</slot>
+        `;
+        return template.content.cloneNode(true);
+    }
+
+    footer() { 
+        const template = document.createElement('template');
+        template.innerHTML = `
+        <slot name="footer">E</slot>
+        `;
+        return template.content.cloneNode(true);
+    }
+
+
+
     connectedCallback() { 
         const post = this.querySelector("a-post");
+        this.root.appendChild(this.header());
         for (let i = 0; i < this.number; i++) { 
             const clonedPost = post.cloneNode(true);
             clonedPost.setAttribute('title', `Title: ${i}`);
             clonedPost.setAttribute('content', `Content: ${i}`);
             this.root.appendChild(clonedPost);
         }
+        this.root.appendChild(this.footer());
     }
 
 }
